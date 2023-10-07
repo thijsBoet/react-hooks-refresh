3db00db8474286eafc1aca56e13378178bb5d910
Matthijs Boet <m.boet@chello.nl>
2023-10-07 15:31:37 +0200

    init commit

---
+++ app/state-hooks/page.jsx
@@ -0,0 +1,21 @@
+'use client';
+import { useState, useReducer } from 'react';
+import styles from '@/app/page.module.css';
+import WatchList from '../components/WatchList';
+
+export default function StateHooks() {
+  const [watchList, setWatchList] = useState([]);
+
+
+	return (
+		<main className={styles.main}>
+			<h1>State Hooks</h1>
+			<p>
+				React provides two hooks for managing state within your apps.
+				useState and useReducer. Let's add state to our watch list below
+				to show how these work.
+			</p>
+			<WatchList />
+		</main>
+	);
+}
